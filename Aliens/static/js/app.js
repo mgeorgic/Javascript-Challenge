88b0c6dfe674c8b55576dcb28d858de0a7b67e61
mgeorgic <79463692+mgeorgic@users.noreply.github.com>
2021-06-13 16:50:20 -0600

    filter by date and city

---
+++ Aliens/static/js/app.js
@@ -0,0 +1,35 @@
+// from data.js
+var tableData = data;
+
+// Setup 
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputField1 = d3.select("#datetime");
+var inputField2 = d3.select("#city");
+var resetbtn = d3.select("#reset-btn");
+var columns = ["datetime", "city", "state", "country", "shape", "comments" ]
+
+// Populate the Data Table for columns names and rows
+var populate = (dataInput) => {
+
+	dataInput.forEach(ufo_sightings => {
+		var row = tbody.append("tr");
+		columns.forEach(column => row.append("td").text(ufo_sightings[column])
+		)
+	});
+}
+
+populate(data)
+
+// Filter by date and city
+button.on("click", () => {
+	d3.event.preventDefault();
+	var inputDate = inputField1.property("value").trim();
+	var inputCity = inputField2.property("value").toLowerCase().trim();
+	// Filter by field matching input value
+	var filterDate = data.filter(data => data.datetime === inputDate);
+	console.log(filterDate)
+	var filterCity = data.filter(data => data.city === inputCity);
+	console.log(filterCity)
+	var filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
+	console.log(filterData)
